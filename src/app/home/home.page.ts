5f396daf92bd9ee5d79b4a979e52d17f1510532d
unknown <dannyro345@gmail.com>
2019-06-19 13:12:42 -0300

    ultima versão

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { PerfilModalPage } from '../perfil-modal/perfil-modal.page';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,10 +12,17 @@ export class HomePage {
   perfis = [];
 
   constructor(public modalController: ModalController) {
+  
   }
 
   likes(perfis) {
     perfis.likes = perfis.likes + 1;
+  }
+
+  remove(perfil) {
+    var i = this.perfis.indexOf(perfil);
+    this.perfis.splice(i, 1);
+    
   }
 
   async modal() {
